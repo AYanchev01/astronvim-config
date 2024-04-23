c27f5f308786777563586c56c348befbad668c60
Anton Yanchev <ayanchev01@gmail.com>
2024-04-23 22:45:48 +0300

    setup colorscheme

---
+++ lua/plugins/astrocommunity.lua
@@ -0,0 +1,9 @@
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.colorscheme.vscode-nvim" },
+  { import = "astrocommunity.colorscheme.tokyonight-nvim" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.gruvbox-nvim" },
+  { import = "astrocommunity.colorscheme.kanagawa-nvim" },
+  { import = "astrocommunity.colorscheme.melange-nvim" },
+}
