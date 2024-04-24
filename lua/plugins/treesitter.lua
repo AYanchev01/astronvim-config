27c8cae168e18bbd3cd0eaa7cd5556e6d6a41cc9
yaa1sf <Anton.Yanchev@bg.bosch.com>
2024-04-24 15:41:30 +0300

    setup treesitter for CAPL files

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,16 +1,23 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
--- Customize Treesitter
-
 ---@type LazySpec
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = function(_, opts)
+    -- Add CAPL file recognition
+    vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
+      pattern = {"*.can", "*.cin"},
+      callback = function()
+        vim.bo.filetype = "c"
+      end,
+    })
     -- add more things to the ensure_installed table protecting against community packs modifying it
     opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
       "lua",
+      "luadoc",
       "vim",
-      -- add more arguments for adding more treesitter parsers
+      "vimdoc",
+      "cpp",
+      "python",
+      "bash",
     })
   end,
 }
