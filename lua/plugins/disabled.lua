688d3ddb70d43f730fa554aaad6f7fc156230fe8
yaa1sf <Anton.Yanchev@bg.bosch.com>
2024-04-25 12:16:38 +0300

    disable some plugins from astronvim

---
+++ lua/plugins/disabled.lua
@@ -0,0 +1,10 @@
+return {
+  { "goolord/alpha-nvim", enabled = false },
+  { "stevearc/aerial.nvim", enabled = false },
+  { "max397574/better-escape.nvim", enabled = false },
+  { "echasnovski/mini.bufremove", enabled = false },
+  { "folke/neodev.nvim", enabled = false },
+  { "stevearc/resession.nvim", enabled = false },
+  { "folke/todo-comments.nvim", enabled = false },
+  { "s1n7ax/nvim-window-picker", enabled = false },
+}
